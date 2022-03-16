c96d6a17654164eaddb75cc381354e2dd75e01ee
Bosteels Kasper (s106631) <kasper.bosteels@student.ap.be>
2022-03-16 14:14:47 +0100

    api call via axios

---
+++ API-CAll.ts
@@ -0,0 +1,22 @@
+const axios = require("axios");
+require("dotenv").config();
+const token = process.env.API_TOKEN;
+export interface IQuote {
+  _id: string;
+  dialog: string;
+  movie: string;
+  character: string;
+  id: string;
+}
+const instance = axios.create({
+  baseURL: "https://the-one-api.dev/v2/",
+  timeout: 1000,
+  headers: { Authorization: `Bearer ${token}` },
+});
+
+async function GetData() {
+  let rawJsonData = await instance.get("/quote");
+  let quotes: IQuote[] = rawJsonData.data.docs;
+  return quotes;
+}
+GetData();
