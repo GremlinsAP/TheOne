9d036504829ebe30b6d10e8df06fe5761fbac947
Elwyn Van der Borght <elwyn.vanderborght@ap.be>
2022-03-17 19:36:37 +0100

    Object Georienteerd App

--- src/pages.ts
+++ src/pages.ts
@@ -1,24 +1,29 @@
-import { app } from "./index";
 import fs from "fs";
 
-const pages: string[] = fs.readdirSync("./views/").map(s => s.replace(".ejs",""));
+export class Pages {
+
+    public static readonly views: string[] = fs.readdirSync("./views/").map(s => s.replace(".ejs", ""));
+
+    public static SetupViews(app: any): void {
+
+        // Page rendering
+        app.use((req: Request, res: any) => {
+            res.type("text/html");
+            let fileName = req.url == "/" ? "index" : req.url.replace("/", "");
+            
+            // Does it contain our filename?
+            if (Pages.views.includes(fileName)) {
+                res.render(fileName);
+                return;
+            }
+
+            // Not found, send 404 page
+            res.status(404);
+            res.render('404');
+        });
+    }
+}
 
 interface Request {
     url: string;
 }
-
-// Page rendering
-app.use((req: Request, res: any) => {
-    res.type("text/html");
-    let fileName = req.url == "/" ? "index" : req.url.replace("/", "");
-
-    // Does it contain our filename?
-    if (pages.includes(fileName)) {
-        res.render(fileName);
-        return;
-    }
-
-    // Not found, send 404 page
-    res.status(404);
-    res.render('404');
-});
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export class Util {
+    // Some class where you can make functions for like sorting, picking a question
+    // To be decided
+}
