9efd49734782062f1ce148252d1b387c24178b99
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-03-25 21:46:18 +0100

    Sessie

--- src/sessionmanager.ts
+++ src/sessionmanager.ts
@@ -1,19 +1,19 @@
 import { Collection, WithId } from "mongodb";
 import { Database } from "./database";
-//import { Session } from "express-session"
+import { Session } from "express-session"
 
 export class SessionManager {
 
     public static async getAllSessions() {
-    
+
         let sessions = await Database.runOnCollection(Database.SESSIONS, async (coll: Collection) => {
-           return await coll.find({}).toArray();
+            return await coll.find({}).toArray();
         });
 
         return sessions;
     }
 
-    /*public static addSession(session:Session) {
-        
-    }*/
+    public static addSession(session: Session) {
+
+    }
 }
