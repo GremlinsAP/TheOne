fdd884b91d16d152499abdb2cf0517bbe03f6a8a
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-03-26 10:33:48 +0100

    Bug Fix + Style

--- src/public/js/quiz-handler.js
+++ src/public/js/quiz-handler.js
@@ -7,17 +7,19 @@ const quizDataOutputMovie = document.getElementById("quiz-data-output-movie");
 
 const outData = {};
 
+if (!outData.movie || !outData.character)    quizSubmit.disabled = true;
+
 characterOptions.forEach(option => option.onclick = () => setSelectedForOption(option, characterOptions, "character"));
 movieOptions.forEach(option => option.onclick = () => setSelectedForOption(option, movieOptions, "movie"));
+
 const setSelectedForOption = (option, list, type) => {
     list.forEach(o => o.style.backgroundColor = "transparent")
     option.style.backgroundColor = "green";
     outData[type] = option.firstChild.textContent;
-    console.log(outData);
 
     if (outData.movie && outData.character) {
-        quizSubmit.disabled = false;
         quizDataOutputCharacter.value = outData["character"];
         quizDataOutputMovie.value = outData["movie"];
+        quizSubmit.disabled = false;
     }
 } 
