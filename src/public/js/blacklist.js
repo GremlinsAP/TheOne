4791bc02d2e2ccda9e7676fbbddc1539cd106ea7
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-04-21 22:40:49 +0200

    Edit quote

--- src/public/js/blacklist.js
+++ src/public/js/blacklist.js
@@ -8,18 +8,56 @@ for (let blacklistItem of blacklists) {
     let editButton = $(blacklistItem).find(".edit-blacklist-item");
     let trashButton = $(blacklistItem).find(".remove-blacklist-item");
 
+
+    let submitEdit = $(blacklistItem).find(".edit-blacklist-submit");
+
+    console.log(submitEdit)
+
     editButton.on("click", (e) => {
-        $(blacklistItem).find(".blacklist-edit").css("display", "block");
+        let editBox = $(blacklistItem).find(".blacklist-edit");
+
+        console.log(editBox.css("display"));
+        editBox.css("display", editBox.css("display") == "flex" ? "" : "flex");
     });
 
     trashButton.on("click", (e) => {
-   
+
         $(blacklistItem).css('-webkit-animation', 'fadeOut 400ms');
         $(blacklistItem).bind('webkitAnimationEnd', () => {
             $(blacklistItem).remove();
         });
         removeRate(quoteId);
-    })
+    });
+
+    submitEdit.on("click", (e) => {
+        let edittextContent = $(blacklistItem).find("textarea");
+        let boxVal = edittextContent.val();
+
+        let valueText = boxVal.trim();
+
+        if (valueText != "") {
+            edittextContent.val("");
+            $(blacklistItem).find(".blacklist-edit").css("display", "");
+            editRate(quoteId, valueText);
+            $(blacklistItem).find(".blacklist-item-reason").text(`Reason: ${valueText}`);
+        }
+    });
+}
+
+const editRate = async (quoteId, editText) => {
+    await fetch("/rate-quote", {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            type: "blacklist",
+            action: "edit",
+            quoteId: quoteId,
+            reason:editText
+        })
+    });
 }
 
 const removeRate = async (quoteId) => {
@@ -34,5 +72,5 @@ const removeRate = async (quoteId) => {
             action: "remove",
             quoteId: quoteId
         })
-    })
+    });
 }
