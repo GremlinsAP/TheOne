c0dc1d2d1aaa1d148319089bd799a4bfb3bb0e2d
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-05-04 14:03:12 +0200

    testcode download

--- src/public/js/favorites.js
+++ src/public/js/favorites.js
@@ -1,16 +1,23 @@
 const mainContainer = document.getElementById("favorites_container");
 const favorites = $(mainContainer).find(".favorites-item");
+const downloadButton = $(mainContainer).find(".favorites-download");
 
 for (let favoritedItem of favorites) {
     let quoteId = favoritedItem.attributes[0].nodeValue;
     let trashButton = $(favoritedItem).find(".remove-favorites-item");
+    
 
     trashButton.on("click", (e) => {
         $(favoritedItem).css('-webkit-animation', 'fadeOut 400ms');
         $(favoritedItem).bind('webkitAnimationEnd', () => $(favoritedItem).remove());
         removeRate(quoteId);
     });
-}
+}  
+
+
+ downloadButton.on("click", (e) => {
+        download(favorites)
+    });
 
 const removeRate = async (quoteId) => {
     await fetch("/rate-quote", {
@@ -25,4 +32,15 @@ const removeRate = async (quoteId) => {
             quoteId: quoteId
         })
     });
+}
+
+const download = async (favorites) => {
+    console.log("\nFile Contents of file before append:",
+    fs.readFileSync("favorites.txt", "utf8"));
+    
+  fs.appendFileSync("favorites.txt", favorites);
+    
+  // Get the file contents after the append operation
+  console.log("\nFile Contents of file after append:",
+         fs.readFileSync("favorites.txt", "utf8"));
 }
