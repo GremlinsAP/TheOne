661de272e5a4ed3c1f99cbc4107a0cf8612e1e0e
KasperBosteels <kasperbosteels@hotmail.com>
2022-05-22 17:23:27 +0200

    string word geparsed naar url vriendelijk

--- src/WebCrawler.ts
+++ src/WebCrawler.ts
@@ -1,29 +1,33 @@
 const baseurl:string = "https://lotr.fandom.com/wiki/";
-const {Scraper,Root,DownloadContent,OpenLinks,CollectContent} = require('nodejs-web-scraper');
+const {Scraper,Root,DownloadContent} = require('nodejs-web-scraper');
+import fs = require('fs');
 export class WebCrawler {
     constructor(){
 
     }
     /*
-    ScrapeImage is een async operatie.
-    . CharacterName moet een Url vriendelijke naam bevatten.
-    . download character image naar public/images/CharacterImages
-    . logs worden in /WebScraperLogs geschreven.
-
-
+    . ScrapeImage is async
+    . Download character image naar \GremlinsAP\public\assets\images\CharacterImages\
+    . Logs worden in \GremlinsAP\webScraperLogs\ geschreven.
+    . Kan zijn dat er geen foto is voor een karakter(bv: Adrahil I).
+    . Namen komen niet altijd overeen met die van het personage(bv: Frodo Baggins => Untitledjk.jpg)
+    . Namen bevatten niet enkel de naam van het karakter.
+    . Extenties zijn niet altijd jpg maar kunnen ook png zijn.
+    
+    
     example:
-    "import"
-
-     let crawl:WebCrawler = new WebCrawler();
-     crawl.ScrapeImage("Frodo_Baggins");
+    import { WebCrawler } from "./WebCrawler";
+    let crawl = new WebCrawler()
+    crawl.ScrapeImage("Adrahil I")
     */ 
     public async ScrapeImage(CharacterName:string){
         const config = {
             baseSiteUrl:baseurl,
-            startUrl:"https://lotr.fandom.com/wiki/"+CharacterName,
+            startUrl:"https://lotr.fandom.com/wiki/"+CharacterName.replaceAll(" ","_"),
             filePath:'./public/assets/images/CharacterImages',
             concurrency: 1,
-            logPath:'./webScraperLogs'
+            logPath:'./webScraperLogs',
+            
         }  
         const scraper = new Scraper(config);
         const root = new Root();
@@ -31,6 +35,6 @@ export class WebCrawler {
         const image = new DownloadContent('aside figure a img',{
         class:'pi-image-thumbnail'});
         root.addOperation(image);
-        await scraper.scrape(root);
+        scraper.scrape(root)
     }
 }
