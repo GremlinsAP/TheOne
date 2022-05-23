2e5ef71af6db8ad9644f7459508d5c5a2ca5b4d2
KasperBosteels <kasperbosteels@hotmail.com>
2022-05-23 15:46:56 +0200

    redoing the testing

--- src/testscraper.ts
+++ src/testscraper.ts
@@ -1,13 +1,13 @@
 import { WebCrawler } from "./WebCrawler";
-import fs from "fs";
 let crawl = new WebCrawler()
-crawl.ScrapeImage("Frodo Baggins")
-crawl.ScrapeImage("Sauron")
-crawl.ScrapeImage("Balin")
-crawl.ScrapeImage("Théodwyn")
-crawl.ScrapeImage("Eärendil")
-crawl.ScrapeImage("Treebeard")
-crawl.ScrapeImage("Witch-King of Angmar")
-crawl.ScrapeImage("Smaug")
-crawl.ScrapeImage("Bilbo_Baggins")
-crawl.ScrapeImage("Gollum")
+async function getImages(){
+    let characters:string[] = ["Gríma Wormtongue","Peregrin Took","Saruman","Mauhúr","Éowyn","Éomer","Lugdush","Denethor II","Celeborn","Meriadoc Brandybuck","Barliman Butterbur","Gamling","Samwise Gamgee","Gimli","Gorbag","Grimbold","Arwen","Elrond","Uglúk","Aragorn II Elessar","Faramir","Legolas","Master of Lake-town","Thorin II Oakenshield","Dwalin","Bofur","Bard","Shelob","Gandalf","Galadriel","Thrór","Frodo Baggins","Sauron","Balin","Théodwyn","Eärendil","Treebeard","Witch-King of Angmar","Bilbo_Baggins","Gollum","King of the Dead","Gothmog (Balrog)","Khamûl"]
+    console.log(characters.length)
+    for (let i = 0; i < characters.length; i++) {
+        await crawl.ScrapeImage(characters[i]);
+    }
+    console.log(characters.length)
+}
+getImages().then(()=>{
+crawl.renameUnsolved();
+})
